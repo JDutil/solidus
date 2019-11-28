e0542275ae0db0f5f36d9c051ff76a0132d017b7
Paweł Świątkowski <pawel.swiatkowski@juul.com>
2019-11-28 13:14:34 +0100

    Add custom scope from Juulio

--- backend/app/models/spree/comment.rb
+++ backend/app/models/spree/comment.rb
@@ -5,12 +5,14 @@ class Spree::Comment < ActiveRecord::Base
   belongs_to :comment_type
 
   default_scope { order('created_at ASC') }
-  
+
+  # custom scope from Juulio
+  scope :fraud, -> { joins(:comment_type).where(spree_comment_types: { name: 'Fraud' }) }
+
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
   #acts_as_voteable
 
   # NOTE: Comments belong to a user
   belongs_to :user
-
 end
